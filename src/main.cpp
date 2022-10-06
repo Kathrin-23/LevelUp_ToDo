6693bdd14438d8abf88ca7b6f68708f1878f2366
Dmitry Morozov <9984398@gmail.com>
2022-10-06 21:08:13 +0300

    TODO Project : Initial commit. Build : Done

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Dmitry Morozov on 6/10/22.
+//
+
+#include "main.hpp"
+
+#include <iostream>
+
+int main() {
+
+    std::cout << "Hello TODO Project!" << std::endl;
+
+    return 0;
+}
