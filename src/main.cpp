23eb0b4a9fe401e4c4dfdce663ac5f6dcb546055
Dmitry Morozov <9984398@gmail.com>
2022-10-06 22:01:40 +0300

    TODO : Structure : Done Tests : Done

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,39 @@
 //
 
 #include "main.hpp"
+#include "commands.hpp"
 
 #include <iostream>
 
-int main() {
+int main(int argc, char *argv[]) {
 
-    std::cout << "Hello TODO Project!" << std::endl;
+//    std::cout << "argc: " << argc << std::endl;
+//
+//    for (int i = 0; i < argc; ++i) {
+//        std::cout << i << " : " << argv[i] << std::endl;
+//    }
+
+    if (argc < 2) {
+        std::cout << "Usage : " << argv[0] << " <command> " << " [Options]" << std::endl;
+        return 1;
+    }
+
+    std::string command = argv[1];
+
+    if (command == "add") {
+        add_todo();
+    } else if (command == "edit") {
+        edit_todo();
+    } else if (command == "del") {
+        del_todo();
+    } else if (command == "list") {
+        list_todo();
+    } else {
+        std::cout << "Command not found ! " << std::endl;
+        return 1;
+    }
+
+    std::cout << "Given Command :  " << command << std::endl;
 
     return 0;
 }
